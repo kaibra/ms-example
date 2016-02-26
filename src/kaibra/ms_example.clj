b9a31e513bb38c69eef5e9e4180382d60f24a6e5
Kai Brandes <kai.brandes@otto.de>
2016-02-26 18:37:57 +0100

    explicitly starting the server

--- src/kaibra/ms_example.clj
+++ src/kaibra/ms_example.clj
@@ -2,9 +2,10 @@
   (:require
     [kaibra.system :as mount-ms]
     [clojure.tools.logging :as log]
-    [kaibra.stateful.server :refer [server]])
+    [kaibra.stateful.server :as server])
   (:gen-class))
 
 (defn -main [& args]
   (log/info "Starting MS-EXAMPLE")
-  (mount-ms/start))
+  (mount-ms/start
+    #'server/server))
