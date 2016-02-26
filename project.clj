3755e4a8fdbcb03cf6953648b9275ad15cd8bec6
Kai Brandes <kai.brandes@otto.de>
2016-02-26 18:26:26 +0100

    making the logging deps first level deps to see the logging in the uberjar

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
   :dependencies [[org.clojure/tools.logging "0.3.1"]
                  [org.clojure/clojure "1.8.0"]
                  [kaibra/ms-httpkit "0.0.1"]
+                 [mount "0.1.9"]
 
-                 [mount "0.1.9"]]
+                 [javax.servlet/servlet-api "2.5"]
+                 [org.slf4j/slf4j-api "1.7.16"]
+                 [ch.qos.logback/logback-core "1.1.5"]
+                 [ch.qos.logback/logback-classic "1.1.5"]]
   :main ^:skip-aot kaibra.ms-example
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :dev     {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                      [org.slf4j/slf4j-api "1.7.16"]
-                                      [ch.qos.logback/logback-core "1.1.5"]
-                                      [ch.qos.logback/logback-classic "1.1.5"]]}})
+  :profiles {:uberjar {:aot :all}})
